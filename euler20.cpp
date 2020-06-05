0d288dc5c605c0847cdfd550da37482594e8be3f
collinshayden <48135191+collinshayden@users.noreply.github.com>
2020-06-05 12:54:50 -0400

    implemented return

--- euler20.cpp
+++ euler20.cpp
@@ -4,25 +4,25 @@
 // Find the sum of the digits in the number 100!
 #include <iostream>
 #include "ttmath/ttmath.h"
+
 using namespace std;
 
-int factorialsum(int n)
+ttmath::Int<20> factorialsum(int n)
 {
-  //to use ttmath::Int<n>, n calculated by the forumula 2^(32*n). n in this case is 20, meaning the class will support up to 2^(32*20)digits
   ttmath::Int<20> digit, sum = 0; 
   ttmath::Int<20> f = 1;
   for (int i = n; i > 0; i--){
     f *= i;
   }
   while (f>0){
-    digit = f % 10;//remainder of modulo division is the last digit of f
+    digit = f % 10;
     sum += digit;
     f /= 10;
-  } cout<<sum<<endl;
+  } return sum;
 }
 
 int main()
 {
-  factorialsum(100);
+  cout<<factorialsum(100)<<endl;
 }
 //output is 648
